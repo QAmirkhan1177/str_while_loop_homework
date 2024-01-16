341db3c99241b2298c88373b2a32311e5258dbc0
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2024-01-16 16:47:43 +0500

    7 tayyor

--- While07.py
+++ While07.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    c=0
+    while i<len(s):
+        if s[i].isdigit() and int(s[i])%2==0:
+            c+=1
+        i+=1
+    return c
+print(main("123dh!@#64"))
