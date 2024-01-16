3419ec55530803e2ff7bc933117c24ca3c39c21b
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2024-01-16 15:10:53 +0500

    3 tayyor

--- While03.py
+++ While03.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    c=0
+    while i<len(s):
+        if not s[i].isalpha() and not s[i].isdigit() and not s[i].isspace():
+            c+=1
+        i+=1              
+    return c
+print(main("bulls1234@*%$&#"))
