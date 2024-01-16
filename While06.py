8925bef7ed99537a185feaf67a79b351e9d3c9aa
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2024-01-16 16:48:16 +0500

    6 tayyor

--- While06.py
+++ While06.py
@@ -7,3 +7,11 @@ def main(s):
     Returns:
         int: return answer
     """
+    i=0
+    c=0
+    while i<len(s):
+        if s[i].lower()!="a" and s[i].lower()!="e" and s[i].lower()!="i" and  s[i].lower()!="o" and s[i].lower()!="u" and not s[i].isdigit():
+            c+=1
+        i+=1
+    return c
+print(main("sdfgaeih2343jklsdf"))    
